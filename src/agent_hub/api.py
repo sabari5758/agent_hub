0d4e2b0b40d5f2be1b48914b4fde214e07538dfd
sabari_arun_pc <sabariaruntm@gmail.com>
2026-03-11 09:29:47 +0530

    prep for deploy

--- src/agent_hub/api.py
+++ src/agent_hub/api.py
@@ -86,4 +86,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000)) 
     
     # Use 0.0.0.0 to make it accessible to the outside world
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
